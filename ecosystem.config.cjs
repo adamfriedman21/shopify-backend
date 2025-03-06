3e2b48d0b794cd4ff98d878f9577e7c755a56c6b
adamfriedman21 <adamfriedman21@gmail.com>
2025-03-06 19:16:10 +0000

    Fixed ecosystem.config.cjs conflict

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,13 +6,12 @@ module.exports = {
       exec_mode: 'fork',         // Ensures the app runs as a single instance
       instances: 1,
       env: {
-        SUPABASE_URL: 'https://biriflndhkpkuedhquvo.supabase.co',  // Your Supabase URL
-        SUPABASE_KEY: 'your-supabase-key-here', // Your Supabase key
-        PORT: 8080,   // Your port number
-        SHOPIFY_API_KEY: 'your-shopify-api-key-here',  // Your Shopify API Key
-        SHOPIFY_API_SECRET: 'your-shopify-api-secret-here',  // Your Shopify API Secret
+        SUPABASE_URL: 'https://biriflndhkpkuedhquvo.supabase.co',  
+        SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpcmlmbG5kaGtwa3VlZGhxdXZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyNTg4NTIsImV4cCI6MjA1NjgzNDg1Mn0.IEKrwxCP8ha3jqG9Ct_CRPJfv4lP4isxqOJsAa53OXo', 
+        PORT: 8080,   
+        SHOPIFY_API_KEY: 'fb4416f164909f1c14d747ebfc64c665',  
+        SHOPIFY_API_SECRET: 'bff59ee85845c80159306ba3572700cc',  
       }
     }
   ]
 };
-
